07c41e43738ebd1e5a00143eacdbd8b7d5d98185
Osiel <exedraalejandro@gmail.com>
2026-03-23 19:28:19 -0600

    init

---
+++ src/Application262E05/Figura262E05.cs
@@ -0,0 +1,16 @@
+﻿public class Figura262E05
+{
+     public static double perimetro  (double lado, double L, double C)
+     {
+        return ((lado*lado)+L+C); 
+    }
+public static double area (double lado, double altura,double L, double C)
+    {
+        return ((L + C) * altura / 2);
+        
+    }
+    public static double volumen (double lado, double altura,double L, double C)
+    {
+        return ((L + C) * (altura / 2)) * 15;
+    }
+}
---
+++ src/ConsoleApp262E05/Program.cs
@@ -0,0 +1,16 @@
+﻿//formula del perimetro es: lado * lado + base L + base C
+// L = Base Larga
+// C = Base Corta
+//Formula del area es: base L + base C * altura / 2
+
+//variables
+double lado = 5;
+double L = 10;
+double C = 6;
+double altura = 4;
+double perimetro = (lado*lado) + L + C;
+double area= (L + C) * altura / 2;
+
+Console.WriteLine($"EL perimetro de un trapecio de {lado}cm en cada lado, con una base larga de {L}cm y una base corta de {C}cm es: {perimetro}cm");
+Console.WriteLine($"EL area de un trapecio de {lado}cm en cada lado, con una base larga de {L}cm, una base corta de {C}cm y una altura de {altura}cm es: {area}cm^2");
+Console.WriteLine($"el volumen de un trapecio base larga de {L}cm, una base corta de {C}cm y una altura de {altura}cm es: {area * altura}cm^3");
